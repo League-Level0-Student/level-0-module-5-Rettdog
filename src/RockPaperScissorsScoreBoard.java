2fb268cfea99a5e048cd30c8009a5fcb4586784f
league <league@wts14.attlocal.net>
2018-03-11 11:31:35 -0700

    RockPaperScissorsScoreBoard

---
+++ src/RockPaperScissorsScoreBoard.java
@@ -0,0 +1,53 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class RockPaperScissorsScoreBoard {
+	public static void main(String[] args) throws IOException {
+		// inside of main
+		Scanner scn = new Scanner(System.in);
+		FileWriter flw = new FileWriter("score.txt");
+		FileReader flr = new FileReader("score.txt");
+		HashMap<String, Integer> scores = new HashMap<String, Integer>();
+		JOptionPane.showMessageDialog(null, "Welcome to score board");
+		JOptionPane.showMessageDialog(null, "To use this program type in someone's \nname to give them a point");
+		JOptionPane.showMessageDialog(null,
+				"If a person wins in the finals, \nput the word \"win\"+space in front of their \nname to give them 2 points");
+		JOptionPane.showMessageDialog(null, "Type \"stop\" to stop the program");
+
+		while (0 == 0) {
+			String nextLine = scn.nextLine();
+
+			if (nextLine.equals("stop")) {
+				break;
+			}
+			if (scn.hasNextLine()) {
+				if (nextLine.substring(0, 4).equals("win ")) {
+					if (scores.containsKey(nextLine.substring(4, nextLine.length()))) {
+						scores.replace(nextLine.substring(4, nextLine.length()),
+								scores.get(nextLine.substring(4, nextLine.length()) + 2));
+
+					} else {
+						scores.put(nextLine.substring(4, nextLine.length()), 2);
+					}
+				} else if (scores.containsKey(nextLine)) {
+					scores.replace(nextLine, scores.get(nextLine) + 1);
+
+				} else {
+					scores.put(nextLine, 1);
+				}
+
+			}
+
+		}
+		System.out.println(scores);
+		// inside of main
+	}
+	// outside of main
+
+	// outside of main
+}
